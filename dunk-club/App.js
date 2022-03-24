4cc8a52490f210a0aa48f8448460e4c1d4dc1c1b
jdnicoll1 <jdnicoll1@gmail.com>
2022-03-24 13:39:17 -0500

    Frontend login structure set up -- getting started screen, login screen, sign up screen

--- dunk-club/App.js
+++ dunk-club/App.js
@@ -9,15 +9,23 @@ import { registerRootComponent } from 'expo';
 // import all screens for drawer navigation
 import Home from './components/Home.js';
 import DunkChat from './components/DunkChat.js';
+import LoginScreen from "./components/Login/LoginScreen.js";
+import SignUpScreen from "./components/Login/SignUpScreen.js";
+import GettingStarted from "./components/Login/GettingStarted.js";
 
 
 const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerPosition="right" initialRouteName="Home" >
+      <Drawer.Navigator drawerPosition="right" initialRouteName="Getting Started" >
+        <Drawer.Screen name="Getting Started" options={{headerShown: false}} component={GettingStarted} />
+        <Drawer.Screen name="Log In" component={LoginScreen} />
+        <Drawer.Screen name="Sign Up" component={SignUpScreen} />
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Dunk Chat" component={DunkChat} />
+        
+        
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ dunk-club/components/Login/LoginScreen.js
@@ -0,0 +1,186 @@
+import React, { Component } from "react";
+import {
+  Button,
+  View,
+  Text,
+  ImageBackground,
+  TextInput,
+  Pressable,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+// next two imports for email and password icons
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import * as Animatable from "react-native-animatable";
+import { Video, AVPlaybackStatus } from "expo-av";
+
+export default function LoginScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      {/* <Video
+        rate={1}
+        shouldPlay
+        isLooping
+        muted={true}
+        source={require("../assets/videos/city_background_trim.mp4")}
+        style={styles.video}
+        resizeMode="cover"
+      /> */}
+
+      <ImageBackground // when background is decided
+        source={require("../../assets/images/homescreen_background.png")}
+        style={styles.image}
+      />
+      <StatusBar barStyle="light-content" />
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Log In</Text>
+      </View>
+
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
+        <Text style={styles.text_footer}>E-mail</Text>
+        <View style={styles.action}>
+          <FontAwesome // email icon and text field
+            name="user-o"
+            color="#05375a"
+            size={20}
+          />
+          <TextInput
+            placeholder="E-mail"
+            style={styles.textInput}
+            autoCapitalize="none"
+          />
+          {/* <Feather name="check-circle" color="green" size={20} /> */}
+        </View>
+        <Text style={[styles.text_footer, { marginTop: 35 }]}>Password</Text>
+        <View style={styles.action}>
+          <FontAwesome // email icon and text field
+            name="lock"
+            color="#05375a"
+            size={20}
+          />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.textInput}
+            autoCapitalize="none"
+          />
+          {/* <Feather name="eye-off" color="grey" size={20} /> */}
+        </View>
+        <View style={styles.button}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Home")}
+            style={[
+              styles.signIn,
+              {
+                borderColor: "#05375a",
+                borderWidth: 1,
+                marginTop: 15,
+                backgroundColor: "#05375a",
+              },
+            ]}
+          >
+            <Text style={[styles.textSign, { color: "#fff" }]}>Log In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Sign Up")}
+            style={[
+              styles.signIn,
+              {
+                borderColor: "#05375a",
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}
+          >
+            <Text style={[styles.textSign, { color: "#05375a" }]}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </View>
+  );
+}
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "#009387",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 4.5,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  text_footer: {
+    color: "#05375a",
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  errorMsg: {
+    color: "#FF0000",
+    fontSize: 14,
+  },
+  button: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+  signIn: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute", // so text won't be pushed underneath
+  },
+  video: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
